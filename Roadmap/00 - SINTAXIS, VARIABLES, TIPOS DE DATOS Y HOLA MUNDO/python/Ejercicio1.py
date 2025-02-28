668606228c541049d74fac9274b041212a9cbe1d
BRU <55289677+ANDRESSOLER0911@users.noreply.github.com>
2025-02-28 16:53:45 -0600

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Ejercicio1.py
@@ -0,0 +1,40 @@
+# https://www.python.org
+
+# este es un comentario de una sola linea
+
+""" este es 
+un comentario 
+ de varias 
+ lineas
+ """
+variable = 1  #$ LAS VARIABLES SON CUALQUIER VALOR QUE SE PUEDE CAMBIAR
+
+
+My_CONSTANT = 1 # NO EXIATEN CONSATANTES EN PHYTON PERO SE REPRESENTAN CON MAYUSCULAS POR CONVENCIOIN
+
+
+#DATOS PRIMITIVOS DE UN LENGUAJE, SON LOS TIPOS DE DATOS QUE SE PUEDEN MANIPULAR
+
+
+MY_int = 1  #ENTEROS
+my_float = 1.1 #FLOAT flotantes osea decimales
+my_bool = True #BOOLEANOS
+my_bool = False #BOOLEANOS
+my_string = "cualquier palabra" #CADENAS DE TEXTO
+my_otyher_string = "mi itra cadena de texto"
+
+
+print( "hola, phyton!")
+ #FUNCION QUE
+# bucle de suma
+
+suma= 0
+for i in range   (1, 6):
+ suma =+ i 
+print(suma)
+
+# Calcular la suma de los primeros 5 números usando un bucle
+suma = 0
+for i in range(1, 6):  
+    suma += i  # Acumulamos la suma paso a paso
+print(suma)  # Salida: 15
